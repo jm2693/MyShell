894de3ba1562c7b457f7c37a462b9fde011a686a
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-04-04 00:55:42 -0400

    making it simpler

--- mysh.c
+++ mysh.c
@@ -8,10 +8,10 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
-
-void run_shell_loop (int mode_fd) {
-    char command[MAX_COMMAND_LENGTH];
-    char *args[MAX_COMMAND_LENGTH-1];
+// shell loop that will constantly check for input and parse arguments
+void run_shell_loop () {
+    char *command;
+    char **args;
     int is_interactive = isatty(STDIN_FILENO);      
 
     while (1) {
@@ -19,9 +19,9 @@ void run_shell_loop (int mode_fd) {
             printf("mysh> ");
         }
 
-        ssize_t bytes_read = read(mode_fd, command, MAX_COMMAND_LENGTH);
+        ssize_t bytes_read = read(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
 
-        ssize_t bytes_read = read(mode_fd, command, MAX_COMMAND_LENGTH);
+        ssize_t bytes_read = read(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
         if (bytes_read < 0) {
             printf("Error reading command\n");
             continue;
@@ -41,24 +41,12 @@ int main(int argc, char const *argv[])
 {
     // this program takes in at most 2 arguments including the program name. any more results in error
     if (argc > 2) {
-        printf("The program, %s, takes in either 0 or 1 arguments\n", argv[0]);
+        printf("The program, %s, takes in either 0 or 1 argument(s)\n", argv[0]);
         return 1;
     }
 
-    // 
-    int input_fd = STDIN_FILENO;
+    // one major loop that runs the shell in both modes
+    run_shell_loop();
 
-    if (argc == 2) {
-        input_fd = open(argv[1], O_RDONLY);
-        if (input_fd < 0) {
-            perror("Error opening file");
-            return 1;
-        }
-    }
-
-    run_shell_loop(input_fd);
-
-    if (argc == 2) close(input_fd);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
